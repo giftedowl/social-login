ce169f4f0b6c6aa54d069389d97b086b2d981748
Gifted Owl <giftedowl@gmail.com>
2024-12-16 21:57:13 -0500

    Initial commit

---
+++ Social Login/Social_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_LoginApp.swift
+//  Social Login
+//
+//  Created by John Lane on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ Social Login/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Social Login
+//
+//  Created by John Lane on 12/16/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Button("Login with Instagram") {
+            
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
